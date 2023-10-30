8a834e430d6cf967db0c574ddcc7b9020d4b71de
GimmeKitties711 <131449161+GimmeKitties711@users.noreply.github.com>
2023-10-29 19:46:17 -0700

    styled portfolio section

--- react-app/src/components/Project.js
+++ react-app/src/components/Project.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 export default function Project(props) {
     return (
-        <div>
-            <p>{props.title}</p>
+        <div className="projectContainer">
+            <p className="projectElem projectTitle">{props.title}</p>
             <img src={props.imgSrc} className="thumbnail" alt={props.title}/>
-            <Link to={props.repo} target="_blank" rel="noopener noreferrer">GitHub repo</Link>
-            <Link to={props.deployed} target="_blank" rel="noopener noreferrer">Deployed application</Link>
+            <Link to={props.repo} className="projectElem nav-link" target="_blank" rel="noopener noreferrer">GitHub repo</Link>
+            <Link to={props.deployed} className="projectElem nav-link" target="_blank" rel="noopener noreferrer">Deployed application</Link>
         </div>
     );
 }
--- react-app/src/components/pages/Portfolio.js
+++ react-app/src/components/pages/Portfolio.js
@@ -10,14 +10,20 @@ import project3 from '../../pics/project_3_thumbnail.png';
 
 export default function Contact() {
     return (
-        <div>
-            <h1>Portfolio</h1>
-            <Project title="Interactive Weather App" imgSrc={challenge6} repo="https://github.com/GimmeKitties711/challenge_6-interactive_weather_forecast_app" deployed="https://gimmekitties711.github.io/challenge_6-interactive_weather_forecast_app/" />
-            <Project title="Organized Note Taker" imgSrc={challenge11} repo="https://github.com/GimmeKitties711/challenge_11-organized_note_taking_app" deployed="https://organized-note-taking-app-ab8f1d884f19.herokuapp.com/" />
-            <Project title="Book Search Engine" imgSrc={challenge21} repo="https://github.com/GimmeKitties711/challenge_21-personalized_book_search_engine_with_graphql" deployed="https://personalized-book-search-app-c1a85de82eb6.herokuapp.com/" />
-            <Project title="Kitty Quest" imgSrc={project1} repo="https://github.com/xqzo/Kitty-Quiz" deployed="https://xqzo.github.io/Kitty-Quiz/" />
-            <Project title="Fit Squad" imgSrc={project2} repo="https://github.com/carlosmb001/fit_squad" deployed="https://fit-squad-805c3e11f44f.herokuapp.com/" />
-            <Project title="Code-E the Code Bot" imgSrc={project3} repo="https://github.com/Cleffy/Best-Code-Bot-Ever" deployed="https://best-code-bot-ever-6c020fa64dc5.herokuapp.com/" />
+        <div className="sectionContainer">
+            <h1 id="portfolio-title">Portfolio</h1>
+            <div className="portfolioContainer">
+                <div id="assignments-container">
+                    <Project title="Interactive Weather App" imgSrc={challenge6} repo="https://github.com/GimmeKitties711/challenge_6-interactive_weather_forecast_app" deployed="https://gimmekitties711.github.io/challenge_6-interactive_weather_forecast_app/" />
+                    <Project title="Organized Note Taker" imgSrc={challenge11} repo="https://github.com/GimmeKitties711/challenge_11-organized_note_taking_app" deployed="https://organized-note-taking-app-ab8f1d884f19.herokuapp.com/" />
+                    <Project title="Book Search Engine" imgSrc={challenge21} repo="https://github.com/GimmeKitties711/challenge_21-personalized_book_search_engine_with_graphql" deployed="https://personalized-book-search-app-c1a85de82eb6.herokuapp.com/" />
+                </div>
+                <div id="projects-container">
+                    <Project title="Kitty Quest" imgSrc={project1} repo="https://github.com/xqzo/Kitty-Quiz" deployed="https://xqzo.github.io/Kitty-Quiz/" />
+                    <Project title="Fit Squad" imgSrc={project2} repo="https://github.com/carlosmb001/fit_squad" deployed="https://fit-squad-805c3e11f44f.herokuapp.com/" />
+                    <Project title="Code-E the Code Bot" imgSrc={project3} repo="https://github.com/Cleffy/Best-Code-Bot-Ever" deployed="https://best-code-bot-ever-6c020fa64dc5.herokuapp.com/" />
+                </div>
+            </div>
         </div>
     );
 }
